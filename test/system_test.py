ff32063ef8aa6adf03901bbf59957aaebedc7bc3
Björn Edström <be@bjrn.se>
2016-01-10 15:39:28 +0100

    Python 3 compat.

--- test/system_test.py
+++ test/system_test.py
@@ -17,42 +17,42 @@ class SystemTest(unittest.TestCase):
     def test_random_source(self):
         out, err = run(['randtool', '-r', '-n', '16'])
         self.assertEquals(16, len(out))
-        self.assertEquals('', err)
+        self.assertEquals(b'', err)
 
         out2, err = run(['randtool', '-r', '-n', '16'])
         self.assertEquals(16, len(out2))
-        self.assertEquals('', err)
+        self.assertEquals(b'', err)
 
         self.assertTrue(out != out2)
 
     def test_seeded_source(self):
         out, err = run(['randtool', '-s', 'foo', '-n', '16'])
         self.assertEquals(16, len(out))
-        self.assertEquals('', err)
+        self.assertEquals(b'', err)
 
         out2, err = run(['randtool', '-s', 'foo', '-n', '16'])
         self.assertEquals(16, len(out2))
-        self.assertEquals('', err)
+        self.assertEquals(b'', err)
 
         self.assertTrue(out == out2)
 
     def test_seeded_source_kat(self):
         out, err = run(['randtool', '-s', 'foo', '-n', '20'])
         self.assertEquals(b'yh1m\xd1r\xe4m\xc0\xc8\xe8\x1dz\x98%\xa9\x94\xf7g\xa0', out)
-        self.assertEquals('', err)
+        self.assertEquals(b'', err)
 
     def test_password_source(self):
         raise NotImplementedError('fixme')
 
     def test_xor(self):
         plaintext = b'foo bar test abcdef 1234567'
-        with file('/tmp/randtool.xortest', 'w') as fobj:
+        with open('/tmp/randtool.xortest', 'wb') as fobj:
             fobj.write(plaintext)
         out, err = run(['randtool', '-s', 'foo', '-n', '5', '--xor', '/tmp/randtool.xortest', '-o', '/tmp/randtool.xortest.enc'])
         self.assertTrue(not err)
         self.assertTrue(not out)
 
-        with file('/tmp/randtool.xortest.enc', 'r') as fobj:
+        with open('/tmp/randtool.xortest.enc', 'rb') as fobj:
             ciphertext = fobj.read()
 
         out, err = run(['randtool', '-s', 'foo', '-n', '5', '--xor', '/tmp/randtool.xortest.enc'])
@@ -61,13 +61,13 @@ class SystemTest(unittest.TestCase):
 
     def test_xor_inplace(self):
         plaintext = b'foo bar test abcdef 1234567'
-        with file('/tmp/randtool.xortest', 'w') as fobj:
+        with open('/tmp/randtool.xortest', 'wb') as fobj:
             fobj.write(plaintext)
         out, err = run(['randtool', '-s', 'foo', '--xor', '/tmp/randtool.xortest', '-o', 'inplace'])
         self.assertTrue(not err)
         self.assertTrue(not out)
 
-        with file('/tmp/randtool.xortest', 'r') as fobj:
+        with open('/tmp/randtool.xortest', 'rb') as fobj:
             ciphertext = fobj.read()
 
         self.assertTrue(plaintext != ciphertext)
@@ -76,7 +76,7 @@ class SystemTest(unittest.TestCase):
         self.assertTrue(not err)
         self.assertTrue(not out)
 
-        with file('/tmp/randtool.xortest', 'r') as fobj:
+        with open('/tmp/randtool.xortest', 'rb') as fobj:
             plaintext2 = fobj.read()
 
         self.assertEquals(plaintext, plaintext2)
